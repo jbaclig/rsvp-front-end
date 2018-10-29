f39db5de0b8ed3471bd81058fb72fe660aad8c43
Jon Baclig <jon@baclig.com>
2018-10-28 23:56:48 -0700

    change to HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import RSVP from './Components/RSVP';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Admin from './Components/Admin';
 import './App.css';
-import createHashHistory from 'history/createHashHistory';
     
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 class App extends Component {
   render() {
     return (
-      <Router history={hashHistory}>
+      <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Route path="/" exact component={RSVP} />
           <Route path="/admin" component={Admin} />
